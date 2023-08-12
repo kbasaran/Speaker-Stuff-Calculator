da0a72139d6fa3d3b62e2b7c8e380be3d2471acb
kbasaran <kbasaran@gmail.com>
2023-08-12 23:08:20 +0200

    better save/load and sound engine
    beep is still blocking the GUI thread

--- speaker_stuff_calculator_0.2_WIP.py
+++ speaker_stuff_calculator_0.2_WIP.py
@@ -20,6 +20,9 @@ from pathlib import Path
 import electroacoustical as eac
 from collections import OrderedDict
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
 # https://realpython.com/python-super/#an-overview-of-pythons-super-function
 # super(super_of_which_class?=this class, in_which_object?=self)
 # The parameterless call to super() is recommended and sufficient for most use cases
@@ -48,20 +51,34 @@ class Settings:
         self.setattr(attr_name, new_val)
 
 
-class BeeperEngine(qtc.QObject):
+class SoundEngine(qtc.QThread):
     def __init__(self, settings):
         super().__init__()
         self.FS = settings.FS
 
     def run(self):
-        # do a test beep with default values
-        self.beep(1, 1000)
+        self.start_stream()
+        # do a start beep
+        self.beep(2, 100)
 
-    @qtc.Slot(float, float)
+    def start_stream(self):
+        self.stream = sd.Stream(samplerate=self.FS, channels=2)
+        self.dtype = self.stream.dtype
+        self.channel_count = self.stream.channels[0]
+        self.stream.start()
+
+
+    @qtc.Slot()
+    def wait(self):
+        self.msleep(1000)
+
+
+    @qtc.Slot(float, str)
     def beep(self, T, freq):
         t = np.arange(T * self.FS) / self.FS
-        y = settings.A_beep * np.sin(t * 2 * np.pi * freq)
-        sd.play(y, samplerate=self.FS)
+        y = np.tile(settings.A_beep * np.sin(t * 2 * np.pi * freq), self.channel_count)
+        y = y.reshape((len(y) // self.channel_count, self.channel_count)).astype(self.dtype)
+        self.stream.write(y)
 
     @qtc.Slot()
     def good_beep(self):
@@ -73,14 +90,17 @@ class BeeperEngine(qtc.QObject):
 
 
 class UserForm(qtc.QObject):
-    signal_field_changed = qtc.Signal(dict)
+    signal_save_clicked = qtc.Signal()
+    signal_load_clicked = qtc.Signal()
+    signal_new_clicked = qtc.Signal()
 
     def __init__(self):
         super().__init__()
-        self.widget = qtw.QWidget()
         self._form_layout = qtw.QFormLayout()
+        self.widget = qtw.QWidget()
         self.widget.set_layout(self._form_layout)
         self.create_form_items()
+        self.make_connections()
 
     def add_line(self):
         line = qtw.QFrame()
@@ -91,7 +111,7 @@ class UserForm(qtc.QObject):
         # self._form_items["line_" + str(n_line)] = line
         self._form_layout.add_row(line)
 
-    def add_title(self, text):
+    def add_title(self, text: str):
         title = qtw.QLabel()
         title.text = text
         title.style_sheet = "font-weight: bold"
@@ -100,7 +120,18 @@ class UserForm(qtc.QObject):
         # self._form_items["title_" + str(n_title)] = title
         self._form_layout.add_row(title)
 
-    def add_spin_box(self, name, description,
+    def add_pushbuttons(self, buttons: dict, vertical=False):
+        layout = qtw.QVBoxLayout() if vertical else qtw.QHBoxLayout()
+        obj = qtw.QWidget()
+        obj.set_layout(layout)
+        for key, val in buttons.items():
+            name = key + "_button"
+            button = qtw.QPushButton(val)
+            self._form_items[name] = button
+            layout.add_widget(button)
+        self._form_layout.add_row(obj)
+
+    def add_spin_box(self, name: str, description: str,
                      data_type,
                      min_max_vals,
                      coeff_to_SI=1,
@@ -118,20 +149,20 @@ class UserForm(qtc.QObject):
         self._form_items[name] = obj
         self._form_layout.add_row(description, obj)
 
-    def add_text_edit_box(self, name, description):
+    def add_text_edit_box(self, name: str, description: str):
         obj = qtw.QLineEdit()
         # obj.setMinimumSize(52, 18)
         self._form_items[name] = obj
         self._form_layout.add_row(description, obj)
 
-    def add_combo_box(self, name, description=None,
-                     item_list=[],
+    def add_combo_box(self, name: str, description=None,
+                     items=[],
                      ):
-        # item_list can contain elements that are tuples.
+        # items can contain elements that are tuples.
         # in that case the second part is user data
         obj = qtw.QComboBox()
         # obj.setMinimumSize(52, 18)
-        for item in item_list:
+        for item in items:
             obj.add_item(*item)  # tuple with userData, therefore *
         self._form_items[name] = obj
         if description:
@@ -139,17 +170,30 @@ class UserForm(qtc.QObject):
         else:
             self._form_layout.add_row(obj)
 
+    def add_choice_buttons(self, name: str, choices: dict, vertical=False):
+        button_group = qtw.QButtonGroup()
+        layout = qtw.QVBoxLayout() if vertical else qtw.QHBoxLayout()
+        obj = qtw.QWidget()
+        obj.set_layout(layout)
 
-    def set_box_value(self, obj, value):
+        for key, val in choices.items():
+            button = qtw.QRadioButton(val)
+            button_group.add_button(button, key)
+            layout.add_widget(button)
+
+        button_group.buttons()[0].set_checked(True)
+        self._form_items[name] = button_group
+        self._form_layout.add_row(obj)
+        
+
+    def set_widget_value(self, obj, value):
         if isinstance(obj, qtw.QComboBox):
             assert isinstance(value, tuple)
-            # assert isinstance(value[0], tuple)
-            # assert isinstance(value[1], tuple)
         else:
             assert type(value) == type(obj.value)
 
 
-    def get_box_value(self, obj):
+    def get_widget_value(self, obj):
         if isinstance(obj, qtw.QComboBox):
             return
         else:
@@ -162,6 +206,7 @@ class UserForm(qtc.QObject):
         self.add_title("Test 1")
         self.add_line()
         self.add_title("Test 2")
+        self.add_pushbuttons({"save": "Save", "load": "Load", "new": "New"})
         self.add_spin_box("fs", "resonance", "double_float", (0, 99))
         self.add_spin_box("n", "amount", "integer", (0, 9))
         self.add_combo_box("coil_options", "Coil options", [("SV", "data1"),
@@ -169,32 +214,15 @@ class UserForm(qtc.QObject):
                                                             ("MEGA", "data3"),
                                                             ])
         self.add_text_edit_box("comments", "Comments..")
+        self.add_choice_buttons("box_type", {0: "small", 1:"large", 2:"off"}, vertical=False)
 
-    def get_user_form_values(self):
-        values = {}
-        for key, obj in self._form_items.items():
-            if isinstance(obj, qtw.QComboBox):
-                obj_value = {"items": [], "current_index": 0}
-                for i_item in range(obj.count):
-                    item_text = obj.item_text(i_item)
-                    item_data = obj.item_data(i_item)
-                    obj_value["items"].append( (item_text, item_data) )
-                obj_value["current_index"] = obj.current_index
-            elif isinstance(obj, qtw.QLineEdit):
-                obj_value = obj.text
-            else:
-                obj_value = obj.value
-            
-            values[key] = obj_value
-
-        return values
-
-    def update_user_form_values(self, values_new):
+    def update_user_form_values(self, values_new: dict):
         no_dict_key_for_widget = set(self._form_items.keys())
         no_widget_for_dict_key = set()
         for key, value_new in values_new.items():                
             try:
                 obj = self._form_items[key]
+
                 if isinstance(obj, qtw.QComboBox):
                     assert isinstance(value_new, dict)
                     obj.clear()
@@ -202,9 +230,17 @@ class UserForm(qtc.QObject):
                     for item in value_new["items"]:
                         obj.add_item(*item)
                     obj.current_index = value_new["current_index"]
+
                 elif isinstance(obj, qtw.QLineEdit):
                     assert isinstance(value_new, str)
                     obj.text = value_new
+
+                elif isinstance(obj, qtw.QPushButton):
+                    pass
+
+                elif isinstance(obj, qtw.QButtonGroup):
+                    obj.button(value_new).set_checked(True)
+
                 else:
                     assert type(value_new) == type(obj.value)
                     obj.value = value_new
@@ -220,10 +256,45 @@ class UserForm(qtc.QObject):
                              f"No data found to update the widget(s): '{no_dict_key_for_widget}'"
                              )
 
+    def get_user_form_values(self) -> dict:
+        values = {}
+        for key, obj in self._form_items.items():
+
+            if isinstance(obj, qtw.QComboBox):
+                obj_value = {"items": [], "current_index": 0}
+                for i_item in range(obj.count):
+                    item_text = obj.item_text(i_item)
+                    item_data = obj.item_data(i_item)
+                    obj_value["items"].append( (item_text, item_data) )
+                obj_value["current_index"] = obj.current_index
+
+            elif isinstance(obj, qtw.QLineEdit):
+                obj_value = obj.text
+
+            elif isinstance(obj, qtw.QPushButton):
+                obj_value = None
+
+            elif isinstance(obj, qtw.QButtonGroup):
+                obj_value = obj.checked_id()
+
+            else:
+                obj_value = obj.value
+            
+            values[key] = obj_value
+
+        return values
+
+    def make_connections(self):
+        self._form_items["load_button"].clicked.connect(self.signal_load_clicked)
+        self._form_items["save_button"].clicked.connect(self.signal_save_clicked)
+        self._form_items["new_button"].clicked.connect(self.signal_new_clicked)
+
 
 class MainWindow(qtw.QMainWindow):
+    signal_new_window = qtc.Signal(dict)
+    signal_beep = qtc.Signal(float, float)
 
-    def __init__(self, settings):
+    def __init__(self, settings, sound_engine, user_form_dict=None):
         super().__init__()
         self.global_settings = settings
         self.create_core_objects()
@@ -231,12 +302,11 @@ class MainWindow(qtw.QMainWindow):
         self.place_widgets()
         self.make_connections()
         self.start_threads()
+        if user_form_dict:
+            self._user_form.update_user_form_values(user_form_dict)
 
     def create_core_objects(self):
-        self._beeper_advanced = BeeperEngine(settings)
-        self._beeper_advanced_thread = qtc.QThread()
-        self._beeper_advanced.move_to_thread(self._beeper_advanced_thread)
-
+        pass
 
     def create_widgets(self):
         self._top_label = qtw.QLabel("Hello World!")
@@ -247,13 +317,10 @@ class MainWindow(qtw.QMainWindow):
         self._beep_freq_display = qtw.QLCDNumber()
         self._beep_advanced_pusbutton = qtw.QPushButton("Beep advanced")
         self._user_form = UserForm()
-        self._save_pusbutton = qtw.QPushButton("Save")
-        self._load_pusbutton = qtw.QPushButton("Load")
-
 
     def place_widgets(self):
-        self._center_widget = qtw.QWidget()
         self._center_layout = qtw.QVBoxLayout()
+        self._center_widget = qtw.QWidget()
         self._center_widget.set_layout(self._center_layout)
         self.set_central_widget(self._center_widget)
 
@@ -262,36 +329,55 @@ class MainWindow(qtw.QMainWindow):
         self._center_layout.add_widget(self._beep_freq_display)
         self._center_layout.add_widget(self._beep_advanced_pusbutton)
         self._center_layout.add_widget(self._user_form.widget)
-        self._center_layout.add_widget(self._save_pusbutton)
-        self._center_layout.add_widget(self._load_pusbutton)
-
 
     def make_connections(self):
         self._beep_advanced_pusbutton.clicked.connect(
-            lambda: self._beeper_advanced.beep(1, self._beep_freq_dial.value)
+            lambda: self.signal_beep.emit(1, self._beep_freq_dial.value)
             )
+        self.signal_beep.connect(sound_engine.beep)
+
         self._beep_freq_display.display(self._beep_freq_dial.value)
         self._beep_freq_dial.valueChanged.connect(self._beep_freq_display.display)
-        self._save_pusbutton.clicked.connect(self.save_preset)
-        self._load_pusbutton.clicked.connect(self.load_preset)
+
+        self._user_form.signal_save_clicked.connect(self.save_preset_file)
+        self._user_form.signal_load_clicked.connect(self.load_preset_file)
+        self._user_form.signal_new_clicked.connect(self.new_window)
 
     def start_threads(self):
-        self._beeper_advanced_thread.start(qtc.QThread.LowPriority)
+    #     self._beeper_advanced_thread.start(qtc.QThread.LowPriority)
+        pass
 
-    def load_preset(self, preset):
+    def save_preset_file(self):
+        save_file_path = os.path.join(os.getcwd(), "my_save.json")
+        try:
+            json_string = json.dumps(self._user_form.get_user_form_values(), indent=4)
+            with open(save_file_path, "wt") as f:
+                f.write(json_string)
+        except Exception as e:
+            raise e
+
+    def load_preset_file(self):
         load_file_path = os.path.join(os.getcwd(), "my_save.json")
         with open(load_file_path, "rt") as f:
             self._user_form.update_user_form_values(json.load(f))
 
-    def save_preset(self):
-        save_file_path = os.path.join(os.getcwd(), "my_save.json")
-        with open(save_file_path, "wt") as f:
-            f.write(json.dumps(self._user_form.get_user_form_values(), indent=4))
+    def new_window(self):
+        self.signal_new_window.emit(self._user_form.get_user_form_values())
 
 
 if __name__ == "__main__":
-    app = qtw.QApplication(sys.argv)  # ne recommendation with qApp
+    app = qtw.QApplication(sys.argv)  # there is a new recommendation with qApp
     settings = Settings()
-    mw = MainWindow(settings)
-    mw.show()
+
+    sound_engine = SoundEngine(settings)
+    sound_engine.start(qtc.QThread.HighPriority)
+
+    def new_window(user_form_dict=None):
+        mw = MainWindow(settings, sound_engine, user_form_dict)
+        mw.signal_new_window.connect(new_window)
+        mw.show()
+        return mw
+
+    new_window()
+    
     app.exec()
