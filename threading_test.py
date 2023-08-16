ee5dd0b8595b17592631427bfdd2fc12d9ba155a
kbasaran <kbasaran@gmail.com>
2023-08-16 21:35:42 +0200

    tests for threading
    both not working as expected. reported in https://stackoverflow.com/questions/76893820/gui-is-blocked-when-it-signals-the-slot-of-a-qthread-object

---
+++ threading_test.py
@@ -0,0 +1,81 @@
+import sys
+import numpy as np
+import sounddevice as sd
+
+from PySide6 import QtWidgets as qtw
+from PySide6 import QtCore as qtc
+
+from __feature__ import snake_case
+from __feature__ import true_property
+
+class SoundEngine(qtc.QThread):
+    def __init__(self):
+        super().__init__()
+        self.FS = 48000
+
+    def run(self):
+        self.start_stream()
+        # do a start beep
+        self.beep(2, 200)
+
+    def start_stream(self):
+        self.stream = sd.Stream(samplerate=self.FS, channels=2)
+        self.dtype = self.stream.dtype
+        self.channel_count = self.stream.channels[0]
+        self.stream.start()
+
+    @qtc.Slot(float, float)
+    def beep(self, T, freq):
+        t = np.arange(T * self.FS) / self.FS
+        y = np.tile(0.1 * np.sin(t * 2 * np.pi * freq), self.channel_count)
+        y = y.reshape((len(y) // self.channel_count, self.channel_count), order='F').astype(self.dtype)
+        y = np.ascontiguousarray(y, self.stream.dtype)
+        self.stream.write(y)
+
+
+class MainWindow(qtw.QMainWindow):
+    signal_beep = qtc.Signal(float, float)
+
+    def __init__(self, sound_engine):
+        super().__init__()
+        self.create_widgets()
+        self.place_widgets()
+        self.make_connections()
+
+    def create_widgets(self):
+        self._beep_freq_dial = qtw.QDial(minimum=200,
+                                         maximum=2000,
+                                         wrapping=False,
+                                         )
+        self._beep_freq_display = qtw.QLCDNumber()
+        self._beep_pusbutton = qtw.QPushButton("Beep")
+
+    def place_widgets(self):
+        self._center_widget = qtw.QWidget()
+        self._center_layout = qtw.QVBoxLayout(self._center_widget)
+        # self._center_widget.set_layout(self._center_layout)
+        self.set_central_widget(self._center_widget)
+
+        self._center_layout.add_widget(self._beep_freq_dial)
+        self._center_layout.add_widget(self._beep_freq_display)
+        self._center_layout.add_widget(self._beep_pusbutton)
+
+    def make_connections(self):
+        self._beep_pusbutton.clicked.connect(
+            lambda: self.signal_beep.emit(1, self._beep_freq_dial.value)
+            )
+        self.signal_beep.connect(sound_engine.beep)
+
+        self._beep_freq_display.display(self._beep_freq_dial.value)
+        self._beep_freq_dial.valueChanged.connect(self._beep_freq_display.display)
+
+if __name__ == "__main__":
+    app = qtw.QApplication(sys.argv)  # there is a new recommendation with qApp
+
+    sound_engine = SoundEngine()
+    sound_engine.start(qtc.QThread.HighPriority)
+
+    mw = MainWindow(sound_engine)
+    mw.show()
+
+    app.exec()
