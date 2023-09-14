88e75927ecf19e20a9f838ad1667bfe66766ae16
kbasaran <kbasaran@gmail.com>
2023-09-14 18:08:59 +0200

    test for low cost offset method working

--- Test apps/test z vs s butter.py
+++ Test apps/test z vs s butter.py
@@ -2,65 +2,52 @@ import scipy.signal as sig
 import matplotlib.pyplot as plt
 import numpy as np
 plt.rcParams["figure.dpi"] = 300
+import sys
+sys.path.append("..")
+import signal_tools
+import time
 
-len_i = 29
-ppo = 6
-i_index = int(len_i // 2)
 
+# Center
 FS = 48000
 nyq = FS / 2
-x_old = np.array(np.arange(0, nyq, nyq/len_i))
-fn_old = (x_old[i_index] * 2**(-1/2/ppo), x_old[i_index] * 2**(1/2/ppo))
-sos_old = sig.butter(4, fn_old, btype="bandpass", output="sos", fs=FS)
-freqs_old = np.abs(sig.sosfreqz(sos_old, x_old, fs=FS))
+ppo = 6
+x = signal_tools.generate_freq_list(100, 20000, ppo)
+i_ref = int(len(x) / 2)
+freq_ref = x[i_ref]
+# print(freq_ref)
+Wn = (freq_ref * 2**(-1/2/ppo), freq_ref * 2**(1/2/ppo))
+sos = sig.butter(4, Wn, btype="bandpass", output="sos", fs=FS)
+freqs = np.abs(sig.sosfreqz(sos, x, fs=FS))
+plt.semilogx(*freqs, label="center")
 
 
-Wn = (i_index / len_i * 2**(-1/2/ppo), i_index / len_i * 2**(1/2/ppo))
-Wn = [f / nyq for f in fn_old]  # same with above
-sos = sig.butter(4, Wn, btype="bandpass", output="sos")
-freqs = np.abs(sig.sosfreqz(sos, len_i))
+for offset in np.arange(len(x)) - i_ref:
 
-plt.plot(freqs_old[1])
-plt.plot(freqs[1])
-
-
-plt.scatter(i_index, np.abs(freqs[1][i_index]))
-plt.grid()
-
-print(freqs_old[0])
-print(freqs[0] / 2 / np.pi * FS)
-
-# print(freqs_old[1])
-# print(freqs[1])
-print(freqs_old[1] / freqs[1])
-print()
-plt.show()
-
-
-
-i_offset = 3
-
-Wn = ((i_index - i_offset) / len_i * 2**(-1/2/ppo), (i_index - i_offset) / len_i * 2**(1/2/ppo))
-# Wn = [f / nyq for f in fn_old]  # same with above
-sos = sig.butter(4, Wn, btype="bandpass", output="sos")
-freqs_2 = np.abs(sig.sosfreqz(sos, len_i))
-
-plt.plot(freqs[1])
-
-
-response = np.zeros(len_i)
-i_write_start = max(0, i_offset)
-i_write_end = min(len_i, len_i + i_offset)
-i_read_start = max(0, -i_offset)
-i_read_end = min(len_i, len_i - i_offset)
-
-print(i_read_start, i_read_end)
-print(i_write_start, i_write_end)
-
-response[i_write_start:i_write_end] = freqs_2[1][i_read_start:i_read_end]
-plt.plot(response)
-plt.grid()
-
-# print(freqs[1])
-# print(response)
-print(freqs[1] / response)
+    # Offset
+    
+    i_offset = int(len(x) / 2) + offset
+    freq_offset = x[i_offset]
+    # print(freq_offset)
+    Wn_offset = (freq_offset * 2**(-1/2/ppo), freq_offset * 2**(1/2/ppo))
+    sos_offset = sig.butter(4, Wn_offset, btype="bandpass", output="sos", fs=FS)
+    freqs_offset = np.abs(sig.sosfreqz(sos_offset, x, fs=FS))
+    plt.semilogx(*freqs_offset, label="offset")
+    
+    # Offset with only shifting
+    
+    response = np.zeros(len(x))
+    i_write_start = max(0, offset)
+    i_write_end = min(len(x), len(x) + offset)
+    i_read_start = max(0, -offset)
+    i_read_end = min(len(x), len(x) - offset)
+    
+    # print(i_read_start, i_read_end)
+    # print(i_write_start, i_write_end)
+    
+    response[i_write_start:i_write_end] = freqs[1][i_read_start:i_read_end]
+    plt.semilogx(x, response, label="offset with shifting")
+    plt.grid()
+    plt.legend()
+    plt.show()
+    print("Error: ", np.sum(np.abs(response - freqs_offset[1])))
